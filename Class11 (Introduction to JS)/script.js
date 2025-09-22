af1dc3780def4ec0feacf5acccb4d72afab47e74
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-09-22 12:13:20 +0530

    js

---
+++ Class11 (Introduction to JS)/script.js
@@ -0,0 +1,43 @@
+console.log('Hello from VS code')
+
+var num = 1000; 
+console.log(num)
+
+var name = "Priyanshi";
+console.log(name)
+
+var surname = 'Sirohi'
+console.log(surname)
+
+var percentage = 33.33
+console.log(percentage)
+
+var isPresent = true;
+console.log(isPresent)
+
+var name = "Shaurya"; // redeclaration is allowed 
+console.log(name)
+
+let num1 = 500
+console.log(num1)
+
+// let num1 = 50; 
+//  Error:'num1' has already been declared
+
+num1 = 'Scaler'
+console.log(num1)
+
+const password = '1234'
+console.log(password)
+
+// password = '0000' 
+// Error: Assignment to constant variable.
+// console.log(password)
+
+let psp;
+
+// const pi;
+// Error:  Missing initializer in const declaration (
+
+
+
