9d93314bd4bc6655bbbfc557059e4634372275b7
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-06 12:50:16 +0530

    avoid hoisting

--- Class14 (Execution Context & Hoisting)/script.js
+++ Class14 (Execution Context & Hoisting)/script.js
@@ -2,8 +2,8 @@
 // console.log(sum)
 // console.log(sum(120, 60))  // 180
 
-console.log(sumExpr)
-console.log(sumExpr(10, 20))
+console.log(sumExpr) // undefined
+// console.log(sumExpr(10, 20)) // Error:  sumExpr is not a function
 
 var a = 20
 console.log(a) // 20
@@ -14,10 +14,8 @@ function sum ( num1, num2){
   return ans
 }
 
-var sumExpr = function(num1, num2){
-  var ans = num1+num2
-  return ans
-}
+
+
 var sum1 = sum(100, 300)
 console.log(sum1) // 400
 var sum2 = sum(a, b)
