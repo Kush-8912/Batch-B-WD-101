db58244a31f15c069a53fd433d95eef58267364d
priyasirohi09 <priyanshi.sirohi@scaler.com>
2025-10-06 11:55:22 +0530

    object

---
+++ Class14 (Execution Context & Hoisting)/object.js
@@ -0,0 +1,11 @@
+let employee = {
+  name : 'Shaurya', 
+  age : 18, 
+  gender : 'male' ,
+  email : 'shaurya@gmail.com' ,
+  isPresent : true
+}
+
+console.log(employee)
+console.log(employee.gender) // Dot Notation
+console.log(employee["email"]) // Bracket Notation
